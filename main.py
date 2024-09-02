ba1b967fbcb874372fb664842eb8b595b435a92f
BEMZlabs <48926280+BEMZ01@users.noreply.github.com>
2024-09-02 02:14:07 +0100

    Update main.py
    Wrong license information

--- main.py
+++ main.py
@@ -2,16 +2,6 @@
 Written by BEMZlabs (github@bemz.info)
 
 Copyright 2024 BEMZlabs
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
-Licence as published by the Free Software Foundation; either version 2 of the Licence, or (at your option) any later 
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public Licence for more details.
-
-You should have received a copy of the GNU General Public Licence along with this program; if not, see 
-<https://www.gnu.org/licenses/>.
 """
 
 import googlemaps
